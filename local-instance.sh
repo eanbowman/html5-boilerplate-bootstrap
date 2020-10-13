374296f47427d169ba2dff47cbc1496eb6de4dca
Lilithe Bowman <eanbowman@gmail.com>
2020-10-13 16:17:15 -0400

    Update local-instance.sh

--- local-instance.sh
+++ local-instance.sh
@@ -1,3 +1,3 @@
 echo The current directory is %CD%
-echo The script is in %CD%\public\
-docker run -d -p 7979:80 -v %CD%\public\:/var/www/html dszymczuk/apache-php7
+echo The script is in %CD%
+docker run -d -p 7979:80 -v %CD%:/var/www/html dszymczuk/apache-php7
