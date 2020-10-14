88e9af6fb7cf20c679955a59f136ba685d95f50d
Lilithe Bowman <eanbowman@gmail.com>
2020-10-13 21:20:06 -0400

    Update local-instance.sh

--- local-instance.sh
+++ local-instance.sh
@@ -1,3 +1,11 @@
-echo The current directory is %CD%
-echo The script is in %CD%\public\
-docker run -d -p 7979:80 -v %CD%\public\:/var/www/html dszymczuk/apache-php7
+PATHTOSERVER=${PWD}
+echo $PATHTOSERVER
+echo docker run -d -p 7979:80 -v $PATHTOSERVER:/var/www/html dszymczuk/apache-php7
+ID=$(docker run -d -p 7979:80 -v $PATHTOSERVER:/var/www/html dszymczuk/apache-php7)
+
+if [ $ID == *"Error"* ]
+then
+	echo "error was found"
+else
+	echo type "docker kill $ID" to stop the instance
+fi
